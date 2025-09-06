9c24fa3ad37c7fa015e1590f3313d732a5d03a28
notBjorn <notbhalu@gmail.com>
2025-09-05 19:29:42 -0700

    Changes to a brach

--- main.cpp
+++ main.cpp
@@ -2,5 +2,7 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "This is supposed to be the second branch"<< std::endl;
+    std::cout << "This is the main motivation for me using github, I want different copies of the same file such that I can experiment on stuff without damaging the initial code" << std::endl;
     return 0;
 }
