b954be5feaffddc656e9e088e79563707d3a1122
notBjorn <notbhalu@gmail.com>
2025-09-09 20:09:16 -0700

    I was able to succesfull move the maze data from the file to the 2D vector. Also created 2 helper functions that let me print the vector or a 2D vector

--- main.cpp
+++ main.cpp
@@ -2,7 +2,21 @@
 #include <fstream>
 #include <vector>
 #include <string>
+// The following is a helper function that lets me print an int vector in a format I desire.
+void printVec(const std::vector<int> &printData ) {
+    for (const int element: printData) {
+        std::cout << element << " ";
+    }
+    std::cout << std::endl;
+}
 
+
+// This is derivative helper function that prints a 2D vector
+void printVecMaze(const std::vector<std::vector<int>> &printData ) {
+    for (std::vector<int> aRow : printData) {
+        printVec(aRow);
+    }
+}
 
 int main() {
     std::ifstream file("./maze.txt");
@@ -15,11 +29,11 @@ int main() {
     std::vector<int> aRow;
     int row = 0;
     while (std::getline(file, line)) {
-
         //getting an extra 0 there for some reason, will diagnose that tomorrow.
 
-        for (auto c : line) {
-            aRow.push_back(c - '0'); // - '0' converts char to int, learned this from the first search result on google...
+        for (auto c: line) {
+            aRow.push_back(c - '0');
+            // - '0' converts char to int, learned this from the first search result on google...
         }
         std::cout << "This is the data in the vector" << std::endl;
 
@@ -28,15 +42,14 @@ int main() {
         maze.push_back(aRow);
         aRow.clear(); // This will empty the temp vector for future use
 
-        // to see if the maze has the correct data
 
-        for (auto i: maze[row]) {
-            std::cout << i << ", ";
-        }
-        std::cout << std::endl;
+
+        //printVec(maze[row]);
 
         row++;
     }
+
+    printVecMaze(maze);
 
     file.close();
     return 0;
